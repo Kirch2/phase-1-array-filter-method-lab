2916c0c19334326d8a6f15c8587a9502ee6e20ac
Chris <Kirch2@gmail.com>
2021-07-22 13:17:54 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    const answer = drivers.filter(function (element) {
+        return string.toUpperCase() === element.toUpperCase()
+    })
+    if (answer) {
+        return answer
+    }
+}
+function fuzzyMatch(drivers, string) {
+    const samechar = drivers.filter(function (element) {
+        return string.substring(0, 1) === element.substring(0, 1)
+    })
+    if (samechar) {
+        return samechar
+    }
+}
+function matchName(drivers, string) {
+    const check = drivers.filter(function (element) {
+        return string === element.name
+    })
+    return check
+}
